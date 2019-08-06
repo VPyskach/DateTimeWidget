7233a6fb0bb0d5d80bc0b32565832394ca24e010
vasya_p <v.pyskach@gmail.com>
2019-08-06 14:29:06 +0300

    first commit

---
+++ app/src/main/java/com/test/datetimewidget/TextClockAppWidget.java
@@ -0,0 +1,28 @@
+package com.test.datetimewidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class TextClockAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+
+    }
+}
+
